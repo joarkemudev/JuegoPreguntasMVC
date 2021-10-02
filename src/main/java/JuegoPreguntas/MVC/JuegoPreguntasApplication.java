ddf7067860b6eb1758c37f9bf5c3cdeb874e2c10
Joarkemu <43143605+Joarkemu@users.noreply.github.com>
2021-10-02 07:25:10 -0500

    Version 1

---
+++ src/main/java/JuegoPreguntas/MVC/JuegoPreguntasApplication.java
@@ -0,0 +1,13 @@
+package JuegoPreguntas.MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegoPreguntasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegoPreguntasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JuegoPreguntas/MVC/JuegoPreguntasApplicationTests.java
@@ -0,0 +1,13 @@
+package JuegoPreguntas.MVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegoPreguntasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
